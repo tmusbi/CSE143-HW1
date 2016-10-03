04582b01fb96b9fe87aa6c2d0547684c70a5412a
Raymond Wu <raymond.wu@t-mobile.com>
2016-10-02 23:02:01 -0700

    rm all project files

--- src/main/java/com/tmus/eds/package-info.java
+++
@@ -1,9 +0,0 @@
-/**********************************************************************
- * Copyright (c) 2016 T-MOBILE USA, INC. All rights reserved.
- * T-MOBILE USA PROPRIETARY/CONFIDENTIAL.
- **********************************************************************/
-/**
- * @author rwu, @date Sep 30, 2016 11:52:24 AM
- *
- */
-package com.tmus.eds;
